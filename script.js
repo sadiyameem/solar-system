0a24f5f4deebb78857e4e2870ac30c02032711da
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-03-25 16:27:38 -0400

    added hover effect on the planets

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function createStars() {
         // increate nummber of stars to 1000
         const star = document.createElement("div");
         star.className= "star";
-        star.style.width = ".1px";
-        star.style.height = ".1px";
+        star.style.width = "1px";
+        star.style.height = "1px";
         star.style.top = Math.random() * 100 + "%";
         star.style.left = Math.random() * 100 + "%";
         container.appendChild(star);
